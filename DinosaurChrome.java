b2a0a537ed0d03e1f02cf45407bab83b38a707a3
Abhishek Prajapatt <prajapattabhishek@gmail.com>
2025-12-15 21:52:11 +0530

    some changes
    some changes
    
    Co-Authored-By: ratnakar879 <249611656+ratnakar879@users.noreply.github.com>

--- DinosaurChrome.java
+++ DinosaurChrome.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-
 public class DinosaurChrome extends JPanel implements ActionListener, KeyListener {
 
     int boardWidth = 750;
